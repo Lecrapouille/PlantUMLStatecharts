c9f12261c2692dd3a0025fde08f44317db2c308c
Quentin Quadrat <lecrapouille@gmail.com>
2022-06-30 21:29:31 +0200

    Update unit tests

--- translator/tests/tests.py
+++ translator/tests/tests.py
@@ -13,42 +13,104 @@ class Transition(object):
     def __str__(self):
         return "Tr " + self.origin + ' ==> ' + self.destination + ' : ' + self.event + ' [ ' + self.guard + ' ] ' + ' / ' + self.action
 
-def run_instruction(inst):
-    if inst.data == 'state_diagram':
-        for c in inst.children:
-            run_instruction(c)
-    elif inst.data in ['comment', 'skin']:
-        pass
-    elif inst.data == 'cpp_code':
-        print("C++ code:", str(inst.children[0]))
-    elif inst.data == 'transition':
-        tr = Transition()
-        if inst.children[1][-1] == '>':
-            tr.origin, tr.destination = inst.children[0].upper(), inst.children[2].upper()
-        else:
-            tr.origin, tr.destination = inst.children[2].upper(), inst.children[0].upper()
-        for i in range(3, len(inst.children)):
-            if inst.children[i].data == 'event':
-                tr.event = ' '.join(inst.children[i].children[:])
-            elif inst.children[i].data == 'guard':
-                tr.guard = inst.children[i].children[0]
-            elif inst.children[i].data == 'action':
-                tr.action = inst.children[i].children[0]
-        print(tr)
-    elif inst.data in ['state_entry', 'state_exit', 'state_comment', 'state_event']:
-        print('State ' + inst.children[0] + ' event: ' + inst.children[1].children[0])
-    else:
-        print("FIXME not yet managed:", inst.data)
-        pass
+def check(exp):
+    if not exp:
+        raise Exception()
+
+def check_AST(root):
+    check(root.data == 'state_diagram')
+    check(len(root.children) == 3)
+
+    # State1 --> State2 : eee [ a[0] + b] / action = 1/3
+    c0 = root.children[0]
+    check(c0.data == 'transition')
+    check(len(c0.children) == 6)
+    check(c0.children[0] == 'State1')
+    check(c0.children[1] == '-->')
+    check(c0.children[2] == 'State2')
+
+    c03 = c0.children[3]
+    check(len(c03.children) == 1)
+    check(c03.data == 'event')
+    check(len(c03.children) == 1)
+    check(c03.children[0] == 'eee')
+
+    c04 = c0.children[4]
+    check(len(c04.children) == 1)
+    check(c04.data == 'guard')
+    check(len(c04.children) == 1)
+    check(c04.children[0] == '[ a[0] + b]')
+
+    c05 = c0.children[5]
+    check(len(c05.children) == 1)
+    check(c05.data == 'uml_action')
+    check(len(c05.children) == 1)
+    check(c05.children[0] == '/ action = 1/3')
+
+    # State2 <-- State3
+    c1 = root.children[1]
+    check(c1.data == 'transition')
+    check(len(c1.children) == 3)
+    check(c1.children[0] == 'State2')
+    check(c1.children[1] == '<--')
+    check(c1.children[2] == 'State3')
+
+    # state State4 {
+    #   [*] -> ON
+    #   ON -> OFF : off
+    #   OFF -> ON : on
+    # }
+    c2 = root.children[2]
+    check(c2.data == 'state_block')
+    check(len(c2.children) == 2)
+    check(c2.children[0] == 'State4')
+    check(c2.children[1].data == 'state_diagram')
+    
+    c21 = c2.children[1]
+    check(len(c21.children) == 3)
+
+    c210 = c21.children[0]
+    check(c210.data == 'transition')
+    check(len(c210.children) == 3)
+    check(c210.children[0] == '[*]')
+    check(c210.children[1] == '->')
+    check(c210.children[2] == 'ON')
+
+    c211 = c21.children[1]
+    check(c211.data == 'transition')
+    check(len(c211.children) == 4)
+    check(c211.children[0] == 'ON')
+    check(c211.children[1] == '->')
+    check(c211.children[2] == 'OFF')
+
+    c2113 = c211.children[3]
+    check(len(c2113.children) == 1)
+    check(c2113.data == 'event')
+    check(len(c2113.children) == 1)
+    check(c2113.children[0] == 'off')
+
+    c212 = c21.children[2]
+    check(c212.data == 'transition')
+    check(len(c212.children) == 4)
+    check(c212.children[0] == 'OFF')
+    check(c212.children[1] == '->')
+    check(c212.children[2] == 'ON')
+
+    c2123 = c212.children[3]
+    check(len(c2123.children) == 1)
+    check(c2123.data == 'event')
+    check(len(c2123.children) == 1)
+    check(c2123.children[0] == 'on')
+
 
 def main():
-    f = open('../statechart.ebnf')
+    f = open('../statecharts.ebnf')
     parser = Lark(f.read())
-    f = open(sys.argv[1])
+    f = open('grammar.plantuml')
     ast = parser.parse(f.read())
     print("AST:", ast.pretty())
-    for inst in ast.children:
-        run_instruction(inst)
+    check(len(ast.children) == 1)
+    check_AST(ast.children[0])
 
 if __name__ == '__main__':
     main()
